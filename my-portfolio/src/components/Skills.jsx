343dd7d85319290e8ba798ed393cedd34cc8574e
Devansh Agarwal <devansh.agarwal01@nagarro.com>
2024-08-01 17:18:18 +0530

    Skills component completed

---
+++ my-portfolio/src/components/Skills.jsx
@@ -0,0 +1,27 @@
+import React from "react";
+import '../styling/Skills.css';
+import { Cursor } from "react-simple-typewriter";
+
+function Skills() {
+    const mySkills = ["Java", "Spring Boot", "Spring", "Hibernate", "JUnit", "Data Structures",  "OOPS", "AWS", "Microservices", "Microfrontends", "Docker", "GIT", "Angular", "React.js", "Node.js", "HTML", "CSS", "Javascript", "MYSQL", "NoSQL"];
+    return (
+        <React.Fragment>
+            <div className="skills_component">
+                <h2>Skills Set<Cursor /></h2>
+                <div className="skills_defined">
+                <div class="container overflow-hidden text-center">
+                    <div class="row row-cols-1 row-cols-sm-2 row-cols-md-3 row-cols-lg-4 row-cols-xl-5 g-3 justify-content-center">
+                        {
+                            mySkills.map((mySkill, index) => (
+                                <div class="col-6 col-sm-4 col-md-3 col-lg-3 col-xl-2 skill" key={index}>{mySkill}</div>
+                            ))
+                        }
+                    </div>
+                    </div>
+                </div>
+            </div>
+        </React.Fragment>
+    );
+}
+
+export default Skills;
--- my-portfolio/src/pages/Portfolio.jsx
+++ my-portfolio/src/pages/Portfolio.jsx
@@ -1,12 +1,14 @@
 import React from "react";
 import { Navbar } from "react-bootstrap";
 import Introduction from "../components/Introduction";
+import Skills from "../components/Skills";
 
 function Portfolio() {
     return ( 
         <React.Fragment>
             {/* <Navbar /> */}
             <Introduction />
+            <Skills />
         </React.Fragment>
      );
 }
