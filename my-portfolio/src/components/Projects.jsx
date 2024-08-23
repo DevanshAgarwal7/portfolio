a79de2d8136f70f7f8f1855225587fe925fe881d
DevanshAgarwal7 <devaagarwal914@gmail.com>
2024-08-23 22:57:13 +0530

    Updated Styling of Project component.

--- my-portfolio/src/components/Projects.jsx
+++ my-portfolio/src/components/Projects.jsx
@@ -60,12 +60,12 @@ function Projects() {
                                     <div className="col-12 col-md-11 col-lg-5 project" key={index}>
                                         <div key={index} className="each_project_grid_item_div">
                                             <div className="each_project_grid_item_header">
-                                                <div className="d-flex flex-column mb-1 justify-content-center align-items-center">
-                                                    <div className="p-2 mx-5"><h6 className="each_project_name">{projectDetail.projectName}</h6></div>
+                                                <div className="d-flex flex-column mb-1 justify-content-center align-items-center" style={{backgroundColor: "yellow"}}>
+                                                    <div className="p-2 mx-5" style={{backgroundColor: "yellow"}}><h6 className="each_project_name">{projectDetail.projectName}</h6></div>
                                                     <div className="p-2 mx-5 each_project_grid_item_project_technology">{projectDetail.projectTechnology}</div>
-                                                    <div className="p-2 mx-5 each_project_grid_item_project_link"><svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-github" viewBox="0 0 16 16">
+                                                    <div className="p-2 mx-5 each_project_grid_item_project_link"><svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" className="bi bi-github" viewBox="0 0 16 16" style={{backgroundColor: "yellow"}}>
                                                         <path d="M8 0C3.58 0 0 3.58 0 8c0 3.54 2.29 6.53 5.47 7.59.4.07.55-.17.55-.38 0-.19-.01-.82-.01-1.49-2.01.37-2.53-.49-2.69-.94-.09-.23-.48-.94-.82-1.13-.28-.15-.68-.52-.01-.53.63-.01 1.08.58 1.23.82.72 1.21 1.87.87 2.33.66.07-.52.28-.87.51-1.07-1.78-.2-3.64-.89-3.64-3.95 0-.87.31-1.59.82-2.15-.08-.2-.36-1.02.08-2.12 0 0 .67-.21 2.2.82.64-.18 1.32-.27 2-.27s1.36.09 2 .27c1.53-1.04 2.2-.82 2.2-.82.44 1.1.16 1.92.08 2.12.51.56.82 1.27.82 2.15 0 3.07-1.87 3.75-3.65 3.95.29.25.54.73.54 1.48 0 1.07-.01 1.93-.01 2.2 0 .21.15.46.55.38A8.01 8.01 0 0 0 16 8c0-4.42-3.58-8-8-8" />
-                                                    </svg>  <a href="" target="_blank">GitHub</a></div>
+                                                    </svg>  <a href="" target="_blank" style={{backgroundColor: "yellow"}}>GitHub</a></div>
                                                 </div>
                                             </div>
                                             <div className="card-body each_project_grid_item_body">
