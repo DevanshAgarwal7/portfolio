1084461a7c07e7e9de5f47e3134e2b6e92eeb473
Devansh Agarwal <devansh.agarwal01@nagarro.com>
2024-08-23 18:13:21 +0530

    Created the navbar and added scrollspy in it

--- my-portfolio/src/pages/Portfolio.jsx
+++ my-portfolio/src/pages/Portfolio.jsx
@@ -1,24 +1,71 @@
 import React from "react";
-import { Navbar } from "react-bootstrap";
 import Introduction from "../components/Introduction";
 import Skills from "../components/Skills";
 import Footer from "../components/Footer";
 import WorkExperience from "../components/WorkExperience";
 import Education from "../components/Education";
 import Projects from "../components/Projects";
+import './Portfolio.css';
 
 function Portfolio() {
-    return ( 
+    return (
         <React.Fragment>
-            {/* <Navbar /> */}
-            <Introduction />
-            <WorkExperience />
-            <Skills />
-            <Education />
-            <Projects />
-            <Footer />
+            <div className="portfolio-page">
+                <div className="navbar-div">
+                    <nav id="portfolio-navbar" className="navbar px-3 mb-3 custom-navbar">
+                        <ul className="nav nav-pills">
+                            <li className="nav-item">
+                                <a className="nav-link" href="#scrollspyHeadingHome">Home</a>
+                            </li>
+                            <li className="nav-item">
+                                <a className="nav-link" href="#scrollspyHeadingWorkExperience">Work Experience</a>
+                            </li>
+                            <li className="nav-item">
+                                <a className="nav-link" href="#scrollspyHeadingSkills">Skills</a>
+                            </li>
+                            <li className="nav-item">
+                                <a className="nav-link" href="#scrollspyHeadingEducation">Education</a>
+                            </li>
+                            <li className="nav-item">
+                                <a className="nav-link" href="#scrollspyHeadingProjects">Projects</a>
+                            </li>
+                            <li className="nav-item">
+                                <a className="nav-link" href="#scrollspyHeadingCertificates">Certificates</a>
+                            </li>
+                            <li className="nav-item">
+                                <a className="nav-link" href="#scrollspyHeadingContactMe">Contact Me</a>
+                            </li>
+                        </ul>
+                    </nav>
+                    <div data-bs-spy="scroll" data-bs-target="#portfolio-navbar" data-bs-root-margin="100px 0px 0px 0px" data-bs-smooth-scroll="true" className="scrollspy-navbar bg-body-tertiary" tabindex="0">
+                        <div className="porfolio-components">
+                            <div id="scrollspyHeadingHome">
+                                <Introduction />
+                            </div>
+                            <div id="scrollspyHeadingWorkExperience">
+                                <WorkExperience />
+                            </div>
+                            <div id="scrollspyHeadingSkills">
+                                <Skills />
+                            </div>
+                            <div id="scrollspyHeadingEducation">
+                                <Education />
+                            </div>
+                            <div id="scrollspyHeadingProjects">
+                                <Projects />
+                            </div>
+                            <div id="scrollspyHeadingCertificates">
+                                {/* < /> */}
+                            </div>
+                            <div id="scrollspyHeadingContactMe">
+                                <Footer />
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
         </React.Fragment>
-     );
+    );
 }
 
 export default Portfolio;
